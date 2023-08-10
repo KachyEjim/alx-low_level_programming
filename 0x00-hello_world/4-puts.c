13088a032d7af19437ca906f92e2854cb4e4cfc0
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-10 15:27:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-    /* Print the specified text */
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+	return (0);
 }
