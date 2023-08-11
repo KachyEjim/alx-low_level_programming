f28f028a0de848308e0a9eec54cb7f958f60840d
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 14:34:42 -0700

    1-last_digit upload file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,7 @@
 
 /**
  * main - Entry point
- *
- * Description: Generates a random number and prints based on the last digit
+ * Description: Generates a random number
  * n: Stores the generated random number
  * x: Stores the last digit of n
  *
@@ -20,11 +19,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	if (n > 5)
+	if (x > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, x);
-	else if (n == 0)
+
+	else if (x == 0)
 		printf("Last digit of %d is 0 and is 0\n", n);
-	else if (n < 6)
+
+	else if (x < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+
 	return (0);
 }
