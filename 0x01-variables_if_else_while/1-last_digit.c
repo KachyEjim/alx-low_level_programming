6aedd7138f582adaad58ba2707bcbbcb358e59bd
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 06:36:41 -0700

    Uploading 0x01-variables_if else_while and also 1-last_digit.c_

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,14 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-
 /** 
  * main function : generates ran num and prints based on the value of the last #
  * n : stores generated random number
  * x : store the last digit of n
  * retuen : returns 0 if code executes suc. and 1 otherwise
 */
-
 int main(void)
 {
 	int n;
@@ -21,7 +19,7 @@ int main(void)
 	x = n % 10;
 	if (n > 5)
 	{
-	 printf ("Last digit of %d is %d and is greater  than 5\n",n,x);
+	 printf ("Last digit of %d is %d and is greater than 5\n",n,x);
 	}
 	else if (n == 0)
 	{
@@ -31,6 +29,5 @@ int main(void)
         {
          printf ("Last digit of %d is %d and is less than 6 and not 0\n",n,x);
         }
-	/* your code goes there */
 	return (0);
 }
