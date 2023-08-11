acc418c8da46e93f96d296957f124dde85511bfe
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-10 17:10:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - entry point
  *
- * Return: Always 1 (Error)
+ * Return: 0 indicates successful execution
  */
 
 int main(void)
 {
-    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
