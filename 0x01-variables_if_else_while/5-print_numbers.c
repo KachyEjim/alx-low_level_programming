a6bd03f594600a4d662b96be15b0de4c21e47242
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 20:04:23 -0700

    5-print_numbers.c  task file upload

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: dispalys number 0to9
+ *
+ * i: stores numbers
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i)
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
