67828444032af67a9e269b7dcd17f1f25ad49659
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 20:15:04 -0700

    6-print_numberz.c task upload

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: dispalys number 0to9
+ *
+ * i: stores numbers
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+        int i = 48;
+
+        while (i <= 57)
+        {
+                putchar(i);
+                i++;
+        }
+
+        putchar(10);
+
+        return (0);
+}
