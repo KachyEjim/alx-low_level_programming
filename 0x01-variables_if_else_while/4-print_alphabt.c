2d6684d29a15b8bfedd0f33755b4805afb47a70e
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 19:52:01 -0700

    4-print_alphabt.c task file upload

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: dispalys alphabets exp q&e
+ *
+ * i: stores characters
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 122)
+	{
+		if (i != 113 || i != 101)
+			putchar(i);
+
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
