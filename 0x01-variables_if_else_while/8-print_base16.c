26312f45fa429d375199aced7a6d19eaec095ec7
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-11 20:54:59 -0700

    upload

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,22 +8,25 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    char c;
+	char c = '0';
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        putchar(c);
-    }
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
 
-    for (c = 'a'; c <= 'f'; c++)
-    {
-        putchar(c);
-    }
+	c = 'a';
 
-    putchar('\n');
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
 
-    return (0);
+	putchar('\n');
+	return (0);
 }
-
