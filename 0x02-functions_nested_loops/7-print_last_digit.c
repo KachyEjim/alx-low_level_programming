b31a9134d00ef47bffa951111d64efff1aa45fb5
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 12:22:02 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,12 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-/*
-	//if (n < 0)
+
+	if (n < 0)
 	n = -n;
-*/
+
 	last_digit = n % 10;
-	_putchar(last_digit + '0');
+	_putchar('0' + last_digit);
 
 	return (last_digit);
 }
