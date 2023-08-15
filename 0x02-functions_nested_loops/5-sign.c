9ee6afd3e30c8444ed7323ddb74114d4cdeb46ad
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 07:03:29 -0700

    5-sign.c task upload

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * description: checks sign of a  number
+ *
+ * @i: parameter that stores the number to be checked
+ *_putchar: function call, print +, 0, - according to numbers sign
+ * Return: returns 1 if positive, 0 if zero and -1 if negative
+ */
+
+int print_sign(int i)
+{
+	if (i > 0)
+	{
+		_putchar('+');
+		_putchar('\n');
+		return (1);
+	}
+
+	else if (i == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+	_putchar('-');
+	_putchar('\n')
+	return (-1)
+}
