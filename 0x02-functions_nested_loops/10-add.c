113aa102b1102b69fbdda4253862118aef2f9bc0
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 15:51:47 -0700

    10-add.c task upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * description: sums two number
+ *
+ * @a: stores the first number
+ * @b: stores the second number
+ *
+ * Return: Returns the sum of a&b
+ */
+
+
+int add(int a, int b);
+{
+	return (a + b);
+}
