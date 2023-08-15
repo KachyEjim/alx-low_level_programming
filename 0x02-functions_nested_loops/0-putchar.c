474eec8fa0c28f7de02f10c351144f097fcaca2f
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 03:24:16 -0700

    0-putchar task upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _puutchar
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char message[] = "_putchar";
+
+	while (message[i] != '\0')
+	{
+		_putchar(message[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/*
+ * Definition of function prototype for _putchar.h 
+ */
+
+int _putchar(char);
