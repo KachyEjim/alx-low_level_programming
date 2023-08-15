1af8a9b20fffbf457b0e8acdb62868744f237c2b
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 16:06:08 -0700

    11-print_to_98.c task upload

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ * description: print fron n to 98
+ *
+ * @n: number to be printed
+ * 
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d,"n);
+		n++;
+	}
+}
