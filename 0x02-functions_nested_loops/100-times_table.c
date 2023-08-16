52f91f5f1ba8cfbcc05e05d480478e53851ad4e2
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 20:33:39 -0700

    100-times_table.c task correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * times_table - Entry point
+ * print_times_table - Entry point
  * description: prints the 9 times table to stdout
  *
+ * @n: input parameter
  * r: stores the rows
  * c: stores the columns
  * i: stores c * r
@@ -14,38 +15,37 @@ void print_times_table(int n)
 {
 	int row;
 	int column;
-	
-	while (n >= 0 && n <= 15)
-	{
-		for (row = 0; row <= n ; row++)
-		{
-			for (column = 0; column <= n; column++)
-			{
-				int result = row * column;
 
-				if (column == 0)
+	if (n < 0 || n > 15)
+		return;
+
+	for (row = 0; row <= n ; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			int result = row * column;
+
+			if (column == 0)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
 				{
+					_putchar(' ');
 					_putchar('0' + result);
 				}
 				else
 				{
-					_putchar(',');
-					_putchar(' ');
-
-					if (result < 10)
-					{
-						_putchar(' ');
-						_putchar('0' + result);
-					}
-					else
-					{
-						_putchar('0' + result / 10);
-						_putchar('0' + result % 10);
-					}
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
 				}
 			}
-
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
