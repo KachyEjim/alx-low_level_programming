aae57d161628fed8bf769c8d4fff8a336281f0be
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-15 23:24:39 -0700

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,18 +13,15 @@
 
 void print_times_table(int n)
 {
-	int row;
-	int column;
+	int row, column, result, temp;
 
 	if (n < 0 || n > 15)
 		return;
-
 	for (row = 0; row <= n ; row++)
 	{
 		for (column = 0; column <= n; column++)
 		{
-			int result = row * column;
-
+			result = row * column;
 			if (column == 0)
 			{
 				_putchar('0' + result);
@@ -32,7 +29,6 @@ void print_times_table(int n)
 			else
 			{
 				_putchar(',');
-
 				if (result < 10)
 				{
 					_putchar(' ');
@@ -42,14 +38,16 @@ void print_times_table(int n)
 				}
 				else if (result > 9 && result < 100)
 				{
-                                        _putchar(' ');
+					_putchar(' ');
 					_putchar('0' + result / 10);
 					_putchar('0' + result % 10);
 				}
 				else
 				{
-                                        _putchar('0' + result / 10);
-                                        _putchar('0' + result % 10);
+					temp  = result / 10;
+					_putchar('0' + temp / 10);
+					_putchar('0' + temp % 10);
+					_putchar('0' + result % 10);
 				}
 			}
 		}
