580ca210994c50b41610850ca6ea0643d2e98882
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-16 21:12:47 -0700

    0x03-debugging first upload

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* description: tests function that prints if integer is positive or negative
+*
+* test: test for the function
+*
+* Return: Always returns 0
+*/
+
+int main(void)
+{
+	int test;
+
+	test = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
