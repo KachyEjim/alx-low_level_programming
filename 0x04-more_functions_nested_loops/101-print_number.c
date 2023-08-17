fc5f4b26651eba8a5f40dca40e072a068154f9aa
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-17 12:03:44 -0700

    101-print_number.c upload

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,13 +12,13 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = n * -1;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar(n % 10 + '0');
 }
