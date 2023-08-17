27e258e60b3c4506de4912d907f8010ec3058cf0
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-17 04:38:35 -0700

    10-print_triangle.c correction upload

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,10 +19,10 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j > size - i - 1; j++)
+			for (j = 0; j < size - i - 1; j++)
 				_putchar(' ');
 
-			for (j = 0; j <= size; j++)
+			for (j = 0; j <= i; j++)
 				_putchar('#');
 
 			_putchar('\n');
