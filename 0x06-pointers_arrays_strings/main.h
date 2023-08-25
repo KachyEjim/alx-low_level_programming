423fc2bb98eb65beb5380a3221dc456f102a88b0
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-25 16:35:40 -0700

    last commit hopefully

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,5 @@ char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char return_number(int n);
+void print_buffer(char *b, int size);
