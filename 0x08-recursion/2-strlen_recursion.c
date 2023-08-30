a0b27f06bf8990d2fe33f6494b1a1aa9b4016f62
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-08-30 11:06:25 -0700

    2-strlen_recursion.c task upload

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ *
+ * @s: string to be size checked
+ *
+ * Returns: returns count
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (!*s)
+	{
+		return (count);
+	}
+
+	count = _strlen_recursion(s + 1);
+	return (++count);
+
+}
