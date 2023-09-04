8529cb70b0aaad4f3ed118f8842d797a3affc540
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-04 14:12:15 -0700

    mandatorytasks last commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ *
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: return array
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	array = (int **) malloc(sizeof(int) * height);
+
+	if (array == 0)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *) malloc(sizeof(int) * width);
+
+		if (array[i] == 0)
+		{
+			i--;
+			for (; i <= 0; i--)
+				free(array[i]);
+
+			free(array);
+			return (0);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid -  frees a 2 dimensional grid
+ *
+ * @grid: 2 dementional grid to be freed
+ * @height: height of the grid
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height;)
+		free(grid[i++]);
+
+	free(grid);
+
+}
