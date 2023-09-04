c9a4234c8f50f0bf61e415f9a7ed539cba268167
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-04 11:35:53 -0700

    2-str_concat.c task upload

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat -  concatenates two strings
+ *
+ * @s1: firt string
+ * @s2: second string
+ *
+ * Return: returns concat
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int t1, t2, i, j = 0;
+
+	if (s1 == 0 || s2 == 0)
+		return (0);
+
+	for (t1 = 0; s1[t1]; t1++)
+		;
+
+	for (t2 = 0; s2[t2]; t2++)
+		;
+
+	concat = (char *) malloc(sizeof(char) * (t1 + t2 + 1));
+
+	if (!concat)
+		return (0);
+
+	for (i = 0; i < t1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; i < t1 + t2 + 1; i++, j++)
+		concat[i] = s2[j];
+
+	return (concat);
+
+}
