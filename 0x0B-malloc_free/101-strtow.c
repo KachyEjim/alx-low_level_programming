ef73c68588ad5dcca62dc3bcef2ca640b8fc502f
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-06 17:14:03 -0700

    101-strtow.c task upload

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,106 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words.
+ *
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
+ *
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, flag = 0, len = 0;
+	char **words;
+
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+		return (NULL);
+
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+		i++;
+	}
+
+	if (flag == 1)
+		len++;
+
+	if (len == 0)
+		return (NULL);
+
+	words = (char **) malloc(sizeof(char *) * (len + 1));
+
+	if (words == NULL)
+		return (NULL);
+
+	util(words, str);
+	words[len] = NULL;
+
+	return (words);
+}
+
+/**
+ * util - a util function for fetching words into an array
+ *
+ * @words: the strings array
+ * @str: the string
+ *
+ */
+
+void util(char **words, char *str)
+{
+	int i = 0, j = 0, start = 0, flag = 0;
+
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			j++;
+			flag = 0;
+		}
+
+		i++;
+	}
+
+	if (flag == 1)
+		create_word(words, str, start, i, j);
+}
+
+/**
+ * create_word - creates a word and insert it into the array
+ *
+ * @words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ *
+ */
+
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
+
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
+		words[index][j] = str[start];
+
+	words[index][j] = '\0';
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,3 +6,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+void create_word(char **words, char *str, int start, int end, int index);
+void util(char **words, char *str);
