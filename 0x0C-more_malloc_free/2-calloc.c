13bdb55486a5ae15c77234b31737c2e227dbb044
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-07 09:58:28 -0700

    correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 	int *ptr;
 
-	if (nmemb  <= 0|| size <= 0)
+	if (nmemb  == 0|| size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
