929af7685922c90ed04266b4a858052c38457595
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-07 17:25:26 -0700

    upload

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,6 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p;
 	unsigned int i, n = new_size;
 	char *oldp = ptr;
+
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
@@ -33,6 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (new_size > old_size)
 		n = old_size;
+
 	for (i = 0; i < n; i++)
 		p[i] = oldp[i];
 	free(ptr);
