038cc466fed8a8d49f690b4f5c2a632c44b6e048
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-07 17:18:38 -0700

    upload

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,4 @@ int *array_range(int min, int max);
 void *allo_space(unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
