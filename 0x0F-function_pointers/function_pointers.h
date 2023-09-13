7d7b165cccf3e98408bf2142c3ab90a8b29f8511
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-13 04:27:23 -0700

    first commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 int _putchar(char c)
 void print_name(char *name, void (*f)(char *));
