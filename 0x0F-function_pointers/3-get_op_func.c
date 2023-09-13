92378d4b7bf1fd580bad6a769502e893a6342e1c
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-13 10:09:36 -0700

    correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,12 +22,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 		};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+		i++;
 	}
 	return (0);
 }
