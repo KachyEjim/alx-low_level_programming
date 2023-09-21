f5af6db5014b1a5d084e24ec3e5b674141910066
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-21 05:16:10 -0700

    upload of task 0 || list.h || README.md || *first push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,25 +4,33 @@
 
 /**
  * print_list - prints all elements of a list_t list.
+ *
  * @h: a pointer to the list to iterato to.
+ *
  * Return: the number of nodes
+ *
  */
+
 size_t print_list(const list_t *h)
 {
 	const list_t *temp;
 	size_t n = 0;
 
-	if (h && h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-	else
+	if (h)
 	{
 		temp = h;
 		while (temp)
 		{
-			printf("[%d] %s\n", temp->len, temp->str);
-			n++;
+			
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%d] %s\n", temp->len, temp->str);
+				n++;
+			}
 			temp = temp->next;
 		}
 	}
