464630d6aebf27b473939a5fde790f4b3af3608c
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-09-26 04:23:27 -0700

    corrections on task 9 and 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	prev = NULL;
 	if (index == i)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
@@ -13,7 +13,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *new, prev;
+	listint_t *temp, *new, *prev;
 	unsigned int i = 0;
 
 	new = malloc(sizeof(listint_t));
