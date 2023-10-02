8a7c616714db7680cae632b97ae24219851ef433
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-10-02 09:29:19 -0700

    correcting betty issues on task 0 & 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		file = open(filename, O_RDWR);
+		file = open(filename, O_TRUNC | O_RDWR);
 		if (file == -1)
 			return (-1);
 	}
