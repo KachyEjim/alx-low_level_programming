7cda7be3089f1355c812dc680206fc703baf84eb
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-10-03 04:05:55 -0700

    Correction of 2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,18 +17,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND | O_EXCL);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 
 	while (text_content[len])
 		len++;
-	len++;
 
-	wlen = write(file, text_content, len);
+	if (text_content != NULL)
+		wlen = write(file, text_content, len);
 
 	close(file);
-	if (wlen == -1)
+	if (wlen == -1 || !text_content)
 		return (-1);
 	return (1);
 }
