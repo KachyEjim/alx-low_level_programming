70ed93a275fc76721c6442203fa6b72d731e1900
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-10-02 21:28:44 -0700

    UPLOAD OF PROJECT TO REPO

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ *
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ *
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t wfile, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	while (text_content(len) != NULL)
+		len++;
+
+	if (text_content != NULL)
+		wfile = write(file, text_content, len);
+
+	close(file);
+
+	if (wfile == -1)
+		return (-1);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error97(int n);
+void error98(ssize_t check, char *fd, int cpfrom, int cpto);
+void error99(ssize_t check, int cpfrom, int cpto, char *file);
+void error100(int check, int file);
 
 #endif /* MAIN_H */
