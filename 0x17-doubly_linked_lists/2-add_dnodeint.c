381d71d1677a40c6f405cd71f3b565067e3836f3
root <root@EJIMOVIC>
2023-11-09 17:19:43 +0100

    Correction of task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,9 +24,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-	if (head)
-		(*head)->prev = new;
 	*head = new;
-	return (*head);
+	if (new->next != NULL)
+		new->next->prev = new;
+	return (new);
 
 }
