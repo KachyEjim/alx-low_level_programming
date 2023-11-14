7960644a29ac9c1ecd3e0baf2cde020153cbfc3a
root <root@EJIMOVIC>
2023-11-14 01:17:49 +0100

    last task

--- 0x18-dynamic_libraries/temp.c
+++ 0x18-dynamic_libraries/temp.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int printf()
+int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 +9\n", 17);
 	write(1, "Congratulations, you win the Jaackpot!\n", 38);
