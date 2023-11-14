558449d7816894c451aa343b3d32ee2b4496e4f1
root <root@EJIMOVIC>
2023-11-14 01:35:44 +0100

    last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp  https://github.com/EJIMVINCENT/alx-low_level_programming/blob/main/0x18-dynamic_libraries/temp.so
-export LD_PRELOAD=/tmp/libtemp.so
+wget -P /tmp https://raw.githubusercontent.com/EJIMVINCENT/alx-low_level_programming/main/0x18-dynamic_libraries/temp.so
+export LD_PRELOAD=/tmp/temp.so
