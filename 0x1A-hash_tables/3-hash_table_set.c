271db85f0e47daca18a437f3f93a923ed8073e9a
root <root@EJIMOVIC>
2023-12-02 02:10:36 +0100

    All task upload

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,34 +4,52 @@
  * hash_table_set - Add or update an element in a hash table.
  *
  * @ht: A pointer to the hash table.
- * @key: The key to add.
+ * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
  *
- * Return: 0 if an error occurs, otherwise return 1
+ * Return: 0 if an error occurs, otherwise return 1.
  *
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *node;
+	char *val;
+	unsigned long int index, i;
 
-	unsigned long int index;
-	hash_node_t *new;
-
-	if (ht == NULL || key == NULL || *key == '\0')
-	{
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	}
+
+	val = strdup(value);
+	if (val == NULL)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val;
+			return (1);
+		}
+	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(val);
 		return (0);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = val;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
-	new->key = strdup(key);
-	new->value = (value != NULL) ? strdup(value) : strdup("");
-
-	new->next = ht->array[index];
-	ht->array[index] = new;
 	return (1);
 }
