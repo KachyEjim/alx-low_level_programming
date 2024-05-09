6c402409f95f65cb1a6be1dfc550dc2dcfb9732f
ejimvincent <ejimovc@gmail.com>
2024-05-09 04:11:15 +0100

    Random check

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 /**
- *
+ * struct listint_s - a struct
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
@@ -44,7 +44,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value)
+listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
